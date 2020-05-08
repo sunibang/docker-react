2f72dd5de7ab5741e0a16deb3b23212557316964
Sunil <Sunil.Sodankur@digio.com.au>
2020-05-08 10:28:07 +1000

    first commit

--- .history/src/App_20200507135116.js
+++ .history/src/App_20200507135116.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Please!!
+          Learn React Please
         </a>
       </header>
     </div>
